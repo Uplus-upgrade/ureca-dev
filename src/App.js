37dc2c200d835a5835e613c1be5aee8262187fb9
cksquf98 <cksquf98@naver.com>
2024-08-18 15:21:48 +0900

    branch test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>안녕하세요</div>;
+  return (
+  <div>안녕하세요
+    <p>찬별 등장</p>
+  </div>
+  );
 }
 
 export default App;
