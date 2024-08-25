ca103ab8ca3c1fc7094b10204b4afaee5f43b4ca
DongHyeonWon <dhe7700@naver.com>
2024-08-25 13:26:20 +0900

    chore: 불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-  <div>안녕하세요
-    <p>찬별 등장</p>
-    <p>33333</p>
-    <p>11111</p>
-  </div>
+    <div>
+      안녕하세요
+      <p>찬별 등장</p>
+      <p>33333</p>
+      <p>11111</p>
+    </div>
   );
 }
 
