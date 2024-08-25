4c3eca83aa36b835998c47dbd0793fe1e7c23dd5
cksquf98 <cksquf98@naver.com>
2024-08-26 00:19:45 +0900

    feat: UserInfo 수정

--- src/layout/GlobalLayout.jsx
+++ src/layout/GlobalLayout.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import * as S from './GlobalLayout.style';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import UserInfo from '../components/UserPhoneInfo/UserInfo';
 
 export default function GlobalLayout({ children }) {
   return (
     <S.GlobalContainer>
       {/* header 자리  */}
       <S.GlobalContent>
-      <Header/>
+        <Header />
+        <UserInfo />
         <Outlet />
       </S.GlobalContent>
     </S.GlobalContainer>
