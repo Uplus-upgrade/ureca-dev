93d2e7d70e5892a54ff7f5cb3f3b509a9cbc88d3
cksquf98 <cksquf98@naver.com>
2024-08-31 17:18:21 +0900

    feat: 데이터 사용량 컴포넌트 생성

--- src/components/UserPhoneInfo/index.jsx
+++ src/components/UserPhoneInfo/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import UserInfo from './UserInfo';
+import UsedDataInfo from './UsedDataInfo';
 
 export default function UserPhoneInfo() {
   return (
     <div>
       <UserInfo />
+      <UsedDataInfo />
     </div>
   );
 }
