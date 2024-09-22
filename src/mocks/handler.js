02fb3b69e05acf447129f06ac6a0e308bfc4f84f
DongHyeonWon <dhe7700@naver.com>
2024-09-22 14:03:30 +0900

    feat: popularMobile api 추가

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -1,8 +1,12 @@
 import { http, HttpResponse } from 'msw';
 import userData from './data/userInfoData.json';
-
+import popularMobile from './data/popularMobile.json';
 export const handlers = [
   http.get('/api/user', () => {
     return HttpResponse.json({ userData });
+  }),
+
+  http.get('/api/popular-mobile', () => {
+    return HttpResponse.json({ popularMobile });
   }),
 ];
